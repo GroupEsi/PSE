a2d1c91f31d359b21850181fa06465318aebafde
unknown <tanguy.charon47@gmail.com>
2015-07-03 00:50:05 +0200

    [US003] Add video on BDD

--- src/PSE/VideoBundle/Controller/SommaireController.php
+++ src/PSE/VideoBundle/Controller/SommaireController.php
@@ -112,4 +112,79 @@ public function addSerieAction(Request $request){
           'form' => $form->createView()
         ));
       }
+
+      public function addVideoAction(Request $request){
+        // Récupère les informations de l'utilisateur connecté depuis la BDD
+        $em = $this->getDoctrine()->getManager();
+        $newVideo = new video();
+        $listeSeries = $em->getRepository('VideoBundle:serie')->findAll();
+
+        // Formulaire pour la modification des infos
+        $form = $this->createFormBuilder()
+        ->add('titre', 'text', array(
+          'label' => 'Titre : ',
+          'data' =>''))
+          ->add('url', 'text', array(
+            'label' => 'Url de l\'iframe : ',
+            'data' =>''))
+            ->add('serie_id', 'choice', array(
+              'label' => 'Description : ', 'choice', array(
+                'choices' => array(
+                  for ($serie in $listeSeries)
+                  {
+                    $serie.id => $serie.titre
+                    }
+                  ),
+                  'multiple' => false,
+                  'expended' => false))
+                  ->add('saison', 'integer', array(
+                    'label' => 'Saison : ',
+                    'data' =>''))
+                    ->add('episode', 'integer', array(
+                      'label' => 'Episode : ',
+                      'data' =>''))
+                      ->add('urlImage', 'text', array(
+                        'label' => 'Url de l\'image : ',
+                        'data' =>'','required' => false))
+                        ->add('sauvegarder', 'submit')
+                        ->getForm();
+
+                  $form->handleRequest($request);
+
+                  // Se lance lorsque le formulaire est soumis
+                  if ($form->isValid()) {
+
+                    $serie = $form->getData();
+
+                    // Enregistre le nouveau titre dans la BDD
+                    $newVideo->setTitre($serie['titre']);
+
+                    // Enregistre le nouveau nbSaisons dans la BDD
+                    $newVideo->setUrl($serie['url']);
+
+                    // Enregistre le nouveau genre dans la BDD
+                    $newVideo->setSerieId($serie['serie_id']);
+
+                    // Enregistre le nouveau description dans la BDD
+                    $newVideo->setSaison($serie['saison']);
+
+                    // Enregistre le nouveau annee dans la BDD
+                    $newVideo->setEpisode($serie['episode']);
+
+                    // Enregistre le nouveau urlImage dans la BDD
+                    $newVideo->setUrlImage($serie['urlImage']);
+
+                    // Applique les modifications de BDD
+                    $em->persist($newVideo);
+                    $em->flush();
+                }
+
+
+
+
+              // Lance la view avec le formulaire en paramètre
+              return $this->render('VideoBundle:Sommaire:addVideo.html.twig', array(
+                'form' => $form->createView()
+              ));
+            }
     }
